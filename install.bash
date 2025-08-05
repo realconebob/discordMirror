021ff683fc5d815d26072a58f5a6f36196ce0b9f
Connor Walstrom <walst110@umn.edu>
2025-08-05 16:42:10 -0500

    Touch up the entire project

--- install.bash
+++ install.bash
@@ -1,41 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env -S bash
 
 # === install.bash ===
 # Installs discordMirror into /opt/, creates a crontab to update the file, and creates a .list so apt can 
-# install the deb. This script should be run only once, and as root.
+# install the deb. This script should be run as root
 
-PATHTO_FOLDER="$(pwd)"
 OPT_LOC="/opt/discordMirror"
+ENV_LOC="/usr/bin/env"
 
-checkRoot() {
-    if [[ "$(whoami)" != "root" ]]; then
-                echo "This script must be run as root"
 
-                location="$(realpath $0)"
-                sudo setpriv --reuid root --regid root --groups root --inh-caps=-all bash "$location"
-                    # su doesn't work if you've disabled root through chsh, but setting the user and group should work on every system regardless of whether root is disabled or not
-                exit $?
-        fi
+SOMEWHAT_REALDIR="$(realpath "$0")"
+FILENAME="${SOMEWHAT_REALDIR##*/}"
+PATHTO="${SOMEWHAT_REALDIR/$FILENAME/}"
 
-    echo "Installing..."
+function runInLocal() {    
+    if [[ "$(pwd)/" != "$PATHTO" ]]; then
+        (cd "$PATHTO" && bash "$FILENAME")
+        return $?
+    fi
 
     return 0
 }
 
-install() {
+function checkInstalled() {
+    if [ -d "$OPT_LOC" ]; then
+        echo "DiscordMirror is already installed"
+        return 1
+    fi
+
+    return 0
+}
+
+function checkRoot() {
+    if [[ "$(whoami)" != "root" ]]; then
+        echo "This script must be ran as root"
+
+        sudo -u root "$ENV_LOC" bash "$FILENAME"
+        return $?
+    fi
+
+    return 0
+}
+
+function install() {
     # Move folder to /opt/ and make it owned by "nobody", a generic unprivileged user
     cp -rvf "$PATHTO_FOLDER" "$OPT_LOC" && \
         chown -R nobody:nogroup "$OPT_LOC" && \
 
         # Install a .list file into /etc/apt/sources.list.d/
         echo "deb [trusted=yes] file:/opt/discordMirror/debs ./" | tee "/etc/apt/sources.list.d/discordMirror.list" && \
-
-        # Install a crontab for the nobody user to occasionally fetch packages
-        (crontab -u nobody -l 2>/dev/null; echo "0 */3 * * * bash /opt/discordMirror/cron/cronUpdate.bash") | crontab -u nobody - && \
-
-        echo "Installed!"
         
+        # Install a crontab for the nobody user to occasionally fetch packages
+        (crontab -u nobody -l 2>/dev/null; echo "0 */3 * * * /usr/bin/bash -S bash /opt/discordMirror/cron/cronUpdate.bash") | crontab -u nobody - && \
+        echo "Installed!";
+        
+    return 0
 }
 
-checkRoot && \
-    install
+runInLocal && \
+    checkInstalled && \
+    checkRoot && \
+    install;
+exit $?
--- scripts/debUpdate.bash
+++ scripts/debUpdate.bash
@@ -1,24 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env -S bash
 
 # === debUpdate.bash ===
 # Update the debian package archive for the local debs folder
 
 DEBS_LOC="../debs/"
 
-debUpdate() {
-    # This is a shitty, hacky solution, but IDC
-    SOMEWHAT_REALDIR="$(realpath $0)"
-    FILENAME="${SOMEWHAT_REALDIR##*/}"
-    PATHTO="${SOMEWHAT_REALDIR/$FILENAME/}"
+SOMEWHAT_REALDIR="$(realpath "$0")"
+FILENAME="${SOMEWHAT_REALDIR##*/}"
+PATHTO="${SOMEWHAT_REALDIR/$FILENAME/}"
+
+function runInLocal() {    
     if [[ "$(pwd)/" != "$PATHTO" ]]; then
-            (cd $PATHTO && bash $FILENAME)
-            exit $?
+        (cd "$PATHTO" && bash "$FILENAME")
+        return $?
     fi
 
-    cd $DEBS_LOC
-
-    dpkg-scanpackages . > "Release" && \
-        dpkg-scanpackages . | gzip -9c > "Packages.gz"
+    return 0
 }
 
-debUpdate
+debUpdate() {
+    cd "$DEBS_LOC" && \
+        dpkg-scanpackages . > "Release" && \
+        dpkg-scanpackages . | gzip -9c > "Packages.gz"
+
+    return $?
+}
+
+runInLocal && \
+    debUpdate
